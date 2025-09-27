38148ae9ddc6f8869dbffce3574577f2726a8c81
jasanchez97 <ja.sanchezmartel@gmail.com>
2025-09-27 14:18:20 +0100

    Añadido texto de reglas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home/Home"
 import BackgroundVideo from "./components/BackgroundVideo/BackgroundVideo"
+import './index.css'
 
 function App() {
 
   return (
     <>
+      <BackgroundVideo />
       <BrowserRouter>
-        <BackgroundVideo />
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="*" element={<Home />} />
