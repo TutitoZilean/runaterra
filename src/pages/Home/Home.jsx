adfb36770eb24c5f00dceff46ded2ac03864991f
jasanchez97 <ja.sanchezmartel@gmail.com>
2025-09-27 11:24:59 +0100

    Agregado pergamino y texto en el header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,11 +7,11 @@ function Rules() {
     return (
         <>
             <Header />
-            <div class="main-menu">
-                <div class="rules">
+            <div className="main-menu">
+                <div className="rules">
                     <p>Se recomienda tener todos los campeones para evitar dolores de cabeza tanto a la hora de elegir regiones o campeones, aun así podéis jugar igual siguiendo las reglas.</p>
 
-                    <p>Como comienzo del minijuego se necesitará crear una personalizada donde jugar partidas con vuestros colegas, mientras más gente llene dicha personalizada mejor será la experiencia de juego, dicho juego consta de dos equipos que obtienen su facción y campeones de runaterra de manera aleatoria elegida por la página y que ésta misma también se encargará de baneará los campeones que salgan seleccionados una vez que se hace cada dos tiradas de azar, todo esto lo hará de forma completamente automática. Cualquier fallo o error me lo comunican.</p>
+                    <p>Como comienzo del minijuego se necesitará crear una personalizada donde jugar partidas con vuestros colegas, mientras más gente llene dicha personalizada mejor será la experiencia de juego, dicho juego consta de dos equipos que obtienen su región y campeones de runaterra de manera aleatoria elegida por la página y que ésta misma también se encargará de banear los campeones que salgan seleccionados una vez que se hace cada dos tiradas de azar, todo esto lo hará de forma completamente automática. Cualquier fallo o error me lo comunican.</p>
 
                     <p>Por temas de diversión y evitar destrozos injustos en las partidas en ningún momento del juego está permitido intercambiarse campeones tanto en un mismo equipo como con el equipo contrario, lo que te toque de campeón es con lo que te quedas.</p>
 
