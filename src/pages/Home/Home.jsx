0a8f1f5e6d2cf770ed0214237e93ae8713599480
jasanchez97 <ja.sanchezmartel@gmail.com>
2025-09-27 15:24:57 +0100

    Preparado para insertar animación para el contenido reglas

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import '../../scripts/rules.jsx'
 import './Home.css'
 
 function Rules() {
@@ -7,7 +8,7 @@ function Rules() {
     return (
         <>
             <Header />
-            <div className="paragraph-container">
+            <div className="paragraph-container" id="paragraph-container">
                 <p className="paragraph-p">Se recomienda tener todos los campeones para evitar dolores de cabeza tanto a la hora de elegir regiones o campeones, aun así podéis jugar igual siguiendo las reglas.</p><br></br>
                 <p className="paragraph-p">Como comienzo del minijuego se necesitará crear una personalizada donde jugar partidas con vuestros colegas, mientras más gente llene dicha personalizada mejor será la experiencia de juego, dicho juego consta de dos equipos que obtienen su región y campeones de runaterra de manera aleatoria elegida por la página y que ésta misma también se encargará de banear los campeones que salgan seleccionados una vez que se hace cada dos tiradas de azar, todo esto lo hará de forma completamente automática. Cualquier fallo o error me lo comunican.</p><br></br>
                 <p className="paragraph-p">Por temas de diversión y evitar destrozos injustos en las partidas en ningún momento del juego está permitido intercambiarse campeones tanto en un mismo equipo como con el equipo contrario, lo que te toque de campeón es con lo que te quedas.</p><br></br>
